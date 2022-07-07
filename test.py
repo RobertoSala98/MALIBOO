031bc2881988bb398b28e7a61eb3a49ec5a3060b
brunoguindani <bruno@guindani.org>
2022-07-07 15:10:58 +0200

    Adding test04 for EI-ML acquisition

--- test.py
+++ test.py
@@ -4,6 +4,13 @@ from bayes_opt import BayesianOptimization as BO
 def black_box_function(x, y):
     return -x ** 2 - (y - 1) ** 2 + 1
 
+def black_box_function_ml(x, y):
+    ret = {}
+    ret['value'] = -x ** 2 - (y - 1) ** 2 + 1
+    ret['something'] = str(x) + "lol"
+    ret['blackbox'] = 2 * x + 3 * y
+    return ret
+
 seed = 1
 n0 = 2
 n_iter = 3
@@ -41,7 +48,16 @@ def test03_dataset_X():
   optimizer.maximize(init_points=n0, n_iter=n_iter)
 
 
+def test04_free_ml():
+  optimizer = BO(f=black_box_function_ml, pbounds={'x': (2, 4), 'y': (-3, 3)},
+                 random_state=seed,
+                 output_path=os.path.join('outputs' ,'test01'))
+  optimizer.maximize(init_points=n0, n_iter=n_iter, acq='ei_ml',
+                     ml_info={'target': 'blackbox', 'bounds': (0, 10)})
+
+
 if __name__ == '__main__':
   perform_test(test01_free)
   perform_test(test02_dataset_Xy)
   perform_test(test03_dataset_X)
+  perform_test(test04_free_ml)
