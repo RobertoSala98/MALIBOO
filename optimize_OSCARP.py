3a35113422098d601ee6fcbf401eac7f4725c8da
RobertoSala98 <roberto10.sala@mail.polimi.it>
2024-01-19 15:04:28 +0100

    preparing file for OSCAR-P

---
+++ optimize_OSCARP.py
@@ -0,0 +1,39 @@
+from maliboo import BayesianOptimization as BO
+
+seed = 21111998
+n0 = 6
+n_iter = 10
+debug = False
+output_path = "./outputs_oscarp_combinatorial_ei_ml"
+delete_previous_output = True
+
+if delete_previous_output:
+    import os; os.system("rm -rf %s" %output_path)
+
+#observed_throughput_ffmpeg-0;avg_response_time_ffmpeg-0;parallelism_ffmpeg-0;
+#observed_throughput_librosa;avg_response_time_librosa;parallelism_librosa;
+#observed_throughput_ffmpeg-1;avg_response_time_ffmpeg-1;parallelism_ffmpeg-1;
+#observed_throughput_ffmpeg-2;avg_response_time_ffmpeg-2;parallelism_ffmpeg-2;
+#observed_throughput_deepspeech;avg_response_time_deepspeech;parallelism_deepspeech;
+#total_time;cost
+
+optimizer = BO(f=None, 
+               pbounds = {'parallelism_ffmpeg-0': [2,4.00001], 
+                          'parallelism_librosa': [2, 6.00001],
+                          'parallelism_ffmpeg-1': [2, 4.00001], 
+                          'parallelism_ffmpeg-2': [2, 8.00001],
+                          'parallelism_deepspeech': [2, 4.00001]},
+               dataset='resources/OSCAR-P_combinatorial.csv', 
+               target_column='-cost',
+               random_state=seed, 
+               output_path=output_path, 
+               debug=debug)
+
+optimizer.maximize(init_points=n0, n_iter=n_iter, acq='ei_ml', acq_info={'ml_target': 'total_time', 'ml_bounds': (0.0, 300.00001)}, memory_queue_len=n_iter+n0)
+
+obtained_max = optimizer.max['target']
+real_max = -0.173553833333333
+
+print("\nObtained min: " + str(round(-obtained_max,2)))
+print("Real min: " + str(round(-real_max,2)))
+print("Error: " + str(round(100*(obtained_max - real_max)/real_max,2)) + " %\n")
