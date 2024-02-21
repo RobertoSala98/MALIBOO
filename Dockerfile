d6512a313b30f74c357391e1208b61b78ceab16b
Roberto Sala <roberto10.sala@mail.polimi.it>
2024-02-21 19:01:35 +0100

    Fixing some details

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,8 @@
-FROM python:3.8
+FROM python:3.9.6
 ENV MY_DIR=/maliboo
 WORKDIR ${MY_DIR}
 COPY . .
+RUN pip install --upgrade pip
+RUN pip3 install -r requirements.txt
 
 CMD bash
--- test_all_config.py
+++ test_all_config.py
@@ -3,105 +3,115 @@ import csv
 from multiprocessing import Pool
 import functools
 
-cores_number = 20
-dataset = "oscarp"
+cores_number = 10
 
-test_file = '%s.yaml' %dataset
+for dataset in ["oscarp", "query26", "stereomatch", "ligen"]:
 
-header = ['ml_bounds', 'ml_target', 'consider_only_true_max', 'error (%)', 'std_dev (%)', 'error_cleaned (%)', 'std_dev_cleaned (%)', 'feasible_values_found (%)', 'avg_time (sec)']
-data = []
+    test_file = '%s.yaml' %dataset
 
-setting = []
+    header = ['ml_bounds', 'ml_target', 'consider_only_true_max', 'error (%)', 'std_dev (%)', 'error_cleaned (%)', 'std_dev_cleaned (%)', 'feasible_values_found (%)', 'avg_time (sec)']
+    data = []
 
-idx_setting = 0
+    setting = []
 
-for ml_bounds in ['indicator', 'probability']:
-    for ml_target in ['None', 'indicator', 'probability', 'sum', 'product']:
-        for consider_only_true_max in [True, False]:
+    idx_setting = 0
 
-            # Modify yaml file
-            with open(test_file, 'r') as file:
-                lines = file.readlines()
+    if dataset in ["oscarp", "ligen"]:
+        algorithms = ['ei', 'DiscreteBO']
+    else:
+        algorithms = ['eic', 'DiscreteBO']
 
-            for idx in range(len(lines)):
+    for algorithm in algorithms:
+        for ml_bounds in ['None']:
+            for ml_target in ['None', 'indicator', 'probability', 'sum', 'product']:
+                for consider_only_true_max in [False]:
 
-                line = lines[idx]
+                    # Modify yaml file
+                    with open(test_file, 'r') as file:
+                        lines = file.readlines()
 
-                output_path = "./outputs/%s/config_%s" %(dataset,idx_setting)
-                if "  output_path: " in line:
-                    lines[idx] = "  output_path: " + output_path + "\n"
+                    for idx in range(len(lines)):
 
-                if "  ml_on_target: " in line: 
+                        line = lines[idx]
 
-                    if ml_target == 'None':
-                        lines[idx] = "  ml_on_target: " + "False\n"
-                    else:
-                        lines[idx] = "  ml_on_target: " + "True\n"
-                        
-                if "  consider_max_only_on_feasible: " in line:
-                    lines[idx] = "  consider_max_only_on_feasible: " + str(consider_only_true_max) + "\n"
+                        output_path = "./outputs/%s/config_%s" %(dataset,idx_setting)
+                        if "  output_path: " in line:
+                            lines[idx] = "  output_path: " + output_path + "\n"
 
-                if "    ml_bounds_type: " in line:
-                    lines[idx] = "    ml_bounds_type: " + ml_bounds + "\n"
+                        if "  acquisition_function: " in line:
+                            lines[idx] = "  acquisition_function: " + algorithm + "\n"
 
-                if "    ml_target_type: " in line:
-                    lines[idx] = "    ml_target_type: " + ml_target + "\n"
+                        if "  ml_on_target: " in line: 
 
-            output_name = "input_files/%s/" %dataset + test_file.split(".yaml")[0] + "_" + str(idx_setting) + ".yaml"
+                            if ml_target == 'None':
+                                lines[idx] = "  ml_on_target: " + "False\n"
+                            else:
+                                lines[idx] = "  ml_on_target: " + "True\n"
+                                
+                        if "  consider_max_only_on_feasible: " in line:
+                            lines[idx] = "  consider_max_only_on_feasible: " + str(consider_only_true_max) + "\n"
 
-            with open(output_name, 'w') as file:
-                file.writelines(lines)
+                        if "    ml_bounds_type: " in line:
+                            lines[idx] = "    ml_bounds_type: " + ml_bounds + "\n"
 
-            idx_setting += 1
+                        if "    ml_target_type: " in line:
+                            lines[idx] = "    ml_target_type: " + ml_target + "\n"
 
-            setting.append([ml_bounds, ml_target, consider_only_true_max, output_name])
+                    output_name = "input_files/%s/" %dataset + test_file.split(".yaml")[0] + "_" + str(idx_setting) + ".yaml"
 
+                    with open(output_name, 'w') as file:
+                        file.writelines(lines)
 
-def split_list(input_list, num_chunks):
-    
-    avg_chunk_size = len(input_list) // num_chunks
-    remainder = len(input_list) % num_chunks
+                    idx_setting += 1
 
-    chunks = []
-    start = 0
-    for i in range(num_chunks):
-        end = start + avg_chunk_size + (1 if i < remainder else 0)
-        chunks.append(input_list[start:end])
-        start = end
-
-    return chunks
+                    setting.append([algorithm, ml_bounds, ml_target, consider_only_true_max, output_name])
 
 
-setting_parallel = split_list(setting, cores_number)
-
-
-def process_batch(settings):
-
-    results = []
-
-    for setting in settings:
-
-        result = setting[:-1]
-        res_sim = test_config(setting[-1])
-        for elem in res_sim:
-            result.append(round(elem,3))
-
-        results.append(result)
-
-    return results
-
-
-with Pool(processes = cores_number) as pool:
-
-    partial_gp = functools.partial(process_batch)
+    def split_list(input_list, num_chunks):
         
-    batch_results_parallel = pool.map(partial_gp, setting_parallel)
+        avg_chunk_size = len(input_list) // num_chunks
+        remainder = len(input_list) % num_chunks
 
-for cc in range(cores_number):
-    data = data + batch_results_parallel[cc] 
+        chunks = []
+        start = 0
+        for i in range(num_chunks):
+            end = start + avg_chunk_size + (1 if i < remainder else 0)
+            chunks.append(input_list[start:end])
+            start = end
+
+        return chunks
 
 
-with open("%s.csv" %dataset, 'w', encoding='UTF8', newline='') as f:
-    writer = csv.writer(f, delimiter=',')
-    writer.writerow(header)
-    writer.writerows(data)  
+    setting_parallel = split_list(setting, cores_number)
+
+
+    def process_batch(settings):
+
+        results = []
+
+        for setting in settings:
+
+            result = setting[:-1]
+            res_sim = test_config(setting[-1])
+            for elem in res_sim:
+                result.append(round(elem,3))
+
+            results.append(result)
+
+        return results
+
+
+    with Pool(processes = cores_number) as pool:
+
+        partial_gp = functools.partial(process_batch)
+            
+        batch_results_parallel = pool.map(partial_gp, setting_parallel)
+
+    for cc in range(cores_number):
+        data = data + batch_results_parallel[cc] 
+
+
+    with open("%s_noBounds_all.csv" %dataset, 'w', encoding='UTF8', newline='') as f:
+        writer = csv.writer(f, delimiter=',')
+        writer.writerow(header)
+        writer.writerows(data)  
